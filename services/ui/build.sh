d9e327fef26e8d3e0adb745f81f4a5515d406dfc
Shahar Tal <chapo12345@gmail.com>
2022-11-04 17:12:12 +0200

    dockerized ui build step so users won't need to install angular to use it

--- services/ui/build.sh
+++ services/ui/build.sh
@@ -12,14 +12,23 @@ md5_directory() {
 }
 
 md5_directory ./code/ >code_md5sum.txt.new
+if [ ! -f code_md5sum.txt ]; then
+    touch code_md5sum.txt
+fi
 
 if cmp --silent -- code_md5sum.txt.new code_md5sum.txt; then
     echo "no new build needed for ui"
 else
     echo "building ui..."
-    cd ./code
-    ng build
-    cd ..
+
+    docker build -t ng-frontend-dist:latest .
+    # prepare output dir
+    rm -rf $(pwd)/dist
+    mkdir $(pwd)/dist
+    # cp output from image
+    docker run --rm -it -v $(pwd)/dist:/tmp/dist ng-frontend-dist mv /var/ng /tmp/dist
+    mv $(pwd)/dist/ng/* $(pwd)/dist/
+    rm -rf $(pwd)/dist/ng
 fi
 
 mv code_md5sum.txt.new code_md5sum.txt
