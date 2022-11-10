b83038cb29f98b0b523ceb4617ddcf3a5d23cbd8
Shahar Tal <chapo12345@gmail.com>
2022-11-11 00:44:06 +0200

    added query params to watch endpoint

--- services/api/source/code/routers/videos/watch.py
+++ services/api/source/code/routers/videos/watch.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Request
 from rds import videos
+from uuid import UUID
 from models import WatchVideoRecord
 
 router = APIRouter()
 
 # watch a video
 @router.get("/", response_model=WatchVideoRecord, response_model_exclude_none=True)
-async def get_watch_video_record(request: Request) -> WatchVideoRecord:
+async def get_watch_video_record(request: Request, user_id: UUID, hash_id: UUID) -> WatchVideoRecord:
     """todo: implement""" # if not private / private but user has premissions, create presigned url on S3
