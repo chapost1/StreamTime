ed1b94deb3d6948ec2413c4a7dc1e0212b806f45
Shahar Tal <chapo12345@gmail.com>
2022-11-11 12:34:05 +0200

    added guidelines comment for delte video use case

--- services/api/source/code/use_cases/videos/delete_video.py
+++ services/api/source/code/use_cases/videos/delete_video.py
@@ -4,6 +4,10 @@ from data_access.abstract import VideosDB
 
 def make_delete_video(videos: VideosDB):
     async def delete_video(authenticated_user_id: UUID, hash_id: UUID) -> None:
-       """todo: implement""" # consider the case of hash_id in unprocessed / videos
+      # verify video / unprocessed_video
+      # delete video from appropriate table
+      # here, user can already recieve 200 OK
+      # delete from S3 [both video and thumbnail]
+      """todo: implement"""
 
     return delete_video
