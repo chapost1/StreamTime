eb7aeb4d06abe1e272fdc210b6eaa0490cc8bdc7
Shahar Tal <chapo12345@gmail.com>
2022-11-12 13:47:30 +0200

    rename get_upload_file_signed_instructions uc file name

--- services/api/source/code/use_cases/videos/__init__.py
+++ services/api/source/code/use_cases/videos/__init__.py
@@ -24,7 +24,7 @@ from use_cases.videos.delete_video import make_delete_video
 delete_video_uc = make_delete_video(videos=videos)
 
 # upload
-from use_cases.videos.get_upload_url import make_get_upload_video_signed_instructions
+from use_cases.videos.get_upload_file_signed_instructions import make_get_upload_video_signed_instructions
 get_upload_video_signed_instructions_uc = make_get_upload_video_signed_instructions(videos=videos, storage=videos_s3_client)
 
 # user
