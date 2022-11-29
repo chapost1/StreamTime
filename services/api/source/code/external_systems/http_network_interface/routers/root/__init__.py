54a73724dde43ba6447322d90193a11e986be0e7
Shahar Tal <chapo12345@gmail.com>
2022-11-29 13:15:47 +0200

    added redirect from root endpoint to health_check endpoint

--- services/api/source/code/external_systems/http_network_interface/routers/root/__init__.py
+++ services/api/source/code/external_systems/http_network_interface/routers/root/__init__.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
 import common.environment as environment
 from .health_check import health_check
+from starlette.responses import RedirectResponse
+
 router = APIRouter(tags=['Root'])
 
 router.add_api_route(environment.HEALTH_CHECK_PATH, health_check)
+
+@router.api_route('/')
+async def redirect_to_health_check() -> None:
+    return RedirectResponse(url=environment.HEALTH_CHECK_PATH)
