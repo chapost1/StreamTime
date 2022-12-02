9cd12f7a92022665760d4b994bdd39741babc5f4
Shahar Tal <chapo12345@gmail.com>
2022-12-02 11:07:40 +0200

    simplified the readability of explore_listed_videos use case

--- services/api/source/code/use_cases/videos/explore_listed_videos.py
+++ services/api/source/code/use_cases/videos/explore_listed_videos.py
@@ -1,22 +1,34 @@
-from typing import List, Union, Callable, Optional
+from typing import List, Union, Callable, Optional, Tuple
 from uuid import UUID
 from entities.videos import Video
 from use_cases.validation_utils import is_anonymous_user
 from external_systems.data_access.rds.abstract import VideosDB
 
+
+def get_explore_visibility_settings(authenticated_user_id: Union[UUID, str], include_my: Optional[bool]) -> Tuple[UUID, UUID]:
+    is_authenticated_user = not is_anonymous_user(authenticated_user_id)
+
+    allow_privates_of_user_id = None
+    exclude_user_id = None
+
+    if is_authenticated_user:
+        # let the authenticated user to view it's own private videos if he will
+        allow_privates_of_user_id = authenticated_user_id
+
+        if not include_my:
+            # user wants to excldue its own videos while exploring
+            exclude_user_id = authenticated_user_id
+    
+    return exclude_user_id, allow_privates_of_user_id
+
+
 def make_explore_listed_videos(videos: VideosDB) -> Callable[[Union[UUID, str], Optional[bool]], List[Video]]:
     async def explore_listed_videos(authenticated_user_id: Union[UUID, str], include_my: Optional[bool] = False) -> List[Video]:
-        is_authenticated_user = not is_anonymous_user(authenticated_user_id)
-
-        if is_authenticated_user:
-            # let the authenticated user to view it's own private videos if he will
-            # it won't appear unless user wants to see it's own videos anyway
-            allow_privates_of_user_id = authenticated_user_id
-
-            if include_my:# user wants to see its own videos while exploring
-                exclude_user_id = None
-            else:# user wants to excldue its videos
-                exclude_user_id = authenticated_user_id
+        
+        exclude_user_id, allow_privates_of_user_id = get_explore_visibility_settings(
+            authenticated_user_id=authenticated_user_id,
+            include_my=include_my
+        )
 
         return await videos.get_listed_videos(
             allow_privates_of_user_id=allow_privates_of_user_id,
