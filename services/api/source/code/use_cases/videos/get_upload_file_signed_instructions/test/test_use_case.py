5910d4d3201b6e1a0dc2185607b71fb881b0f386
Shahar Tal <chapo12345@gmail.com>
2022-12-11 01:33:20 +0200

    use case mock storage to spy call kwargs

--- services/api/source/code/use_cases/videos/get_upload_file_signed_instructions/test/test_use_case.py
+++ services/api/source/code/use_cases/videos/get_upload_file_signed_instructions/test/test_use_case.py
@@ -18,8 +18,6 @@ object_key = f'{user_id}/{hash_id}'
 
 mock_host = 'https://mock.com'
 
-storage = StorageTestClient(host=mock_host)
-
 MP4 = 'mp4'
 
 @pytest.mark.asyncio
@@ -35,9 +33,13 @@ async def test_returns_expected_structure_with_returned_values_in_internals():
         return_value=awaitable_returns_hash_id
     )
 
+    storage = StorageTestClient(host=mock_host)
+
+    spy_storage = AsyncMock(wraps=storage)
+
     result = await use_case(
         database=mock_database,
-        storage=storage,
+        storage=spy_storage,
         assert_file_content_type_fn=lambda file_content_type: ...,
         generate_new_video_hash_id_fn=spy_generate_new_video_hash_id,
         authenticated_user_id=user_id,
@@ -51,6 +53,11 @@ async def test_returns_expected_structure_with_returned_values_in_internals():
         }
     )
 
+    spy_storage.get_upload_file_signed_instructions.assert_called_once_with(
+        item_relative_path=object_key,
+        file_content_type=MP4
+    )
+
     # assert helpers called with args
     spy_generate_new_video_hash_id.assert_called_with(
         database=mock_database,
@@ -62,6 +69,8 @@ async def test_returns_expected_structure_with_returned_values_in_internals():
 async def test_propragate_exception_of_assert_file_content_type_fn():
 
     raise_input_error = Mock(side_effect=InputError)
+
+    storage = StorageTestClient(host=mock_host)
 
     try:
         await use_case(
