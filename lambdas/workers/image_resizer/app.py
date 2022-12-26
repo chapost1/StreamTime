52fd1fb569893613855f9ed81c4f0c8e632998b2
Shahar Tal <chapo12345@gmail.com>
2022-12-26 19:29:42 +0200

    cleaner image_resizer lambda main code

--- lambdas/workers/image_resizer/app.py
+++ lambdas/workers/image_resizer/app.py
@@ -1,19 +1,40 @@
 from typing import Tuple
-import json
-import boto3
 import os
+import boto3
+import json
 from PIL import Image
 
 
 s3Client = boto3.client('s3')
 
 
-def resize_image(image_path: str, resized_path: str, size: Tuple[int, int]):
-    """Resize image locally"""
+def clear_local_tmp_file_cache(file_path: str):
+    """
+    Clear the /tmp folder
+    Lambda shaers the /tmp folder between invocations
+    """
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
-    with Image.open(image_path) as image:
-        image.thumbnail(size)
-        image.save(resized_path)
+
+def download_file_from_s3(bucket: str, key: str, file_path: str):
+    """Download a file from S3"""
+    with open(file_path, 'wb') as f:
+        s3Client.download_fileobj(bucket, key, f)
+
+
+def upload_file_to_s3(bucket: str, key: str, file_path: str, ACL: str = 'private') -> dict:
+    """Upload a file to S3"""
+    with open(file_path, 'rb') as f:
+        resp = s3Client.put_object(Body=f, Bucket=bucket, Key=key, ACL=ACL)
+        return resp
+
+
+def resize_image(source_image_path: str, destination_image_path: str, new_size: Tuple[int, int]) -> None:
+    """Resize image to fit in the given size while keeping the aspect ratio"""
+    with Image.open(source_image_path) as image:
+        image.thumbnail(new_size)
+        image.save(destination_image_path)
 
 
 def lambda_handler(event, context):
@@ -24,35 +45,31 @@ def lambda_handler(event, context):
     """
 
     try:
-        ACL = event.get('ACL', 'private')
-        source_bucket = event['source_bucket']
-        dest_bucket = event['dest_bucket']
-        source_file_key = event['source_file_key']
-        dest_file_key = event['dest_file_key']
-        file_name = source_file_key.split('/')[-1]
+        original_file_key = event['source_file_key']
+        file_name = original_file_key.split('/')[-1]
         temp_file_path = f'/tmp/{file_name}'
-        if os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
+        new_file_path = f'/tmp/modified_{file_name}'
 
-        with open(temp_file_path, 'wb') as f:
-            s3Client.download_fileobj(source_bucket, source_file_key, f)
+        clear_local_tmp_file_cache(file_path=temp_file_path)
 
-        new_file_path = f'/tmp/resized_{file_name}'
-        if os.path.exists(new_file_path):
-            os.remove(new_file_path)
+        clear_local_tmp_file_cache(file_path=new_file_path)
+
+        download_file_from_s3(bucket=event['source_bucket'], key=original_file_key, file_path=temp_file_path)
 
         resize_image(
-            temp_file_path,
-            new_file_path,
-            tuple(event['new_size'])
+            source_image_path=temp_file_path,
+            destination_image_path=new_file_path,
+            size=tuple(event['new_size'])
         )
 
-        print('Going to upload resized image: ' + dest_bucket + '/' + dest_file_key)
-        with open(new_file_path, "rb") as f:
-            resp = s3Client.put_object(Body=f, Bucket=dest_bucket, Key=dest_file_key, ACL=ACL)
-            print(resp)
-            print('Resized imaged has been uploaded')
-
+        print('Going to upload resized image: ' + event['dest_bucket'] + '/' + event['dest_file_key'])
+        upload_file_to_s3(
+            bucket=event['dest_bucket'],
+            key=event['dest_file_key'],
+            file_path=new_file_path,
+            ACL=event.get('ACL', 'private')
+        )
+        print('Resized imaged has been uploaded')
 
     except Exception as e:
         print('Exception')
