b2ebb3f014121cd6518ebd8f64b1a0d577e83d16
Shahar Tal <chapo12345@gmail.com>
2022-12-27 23:04:34 +0200

    garbace_collector | job_scheduler semantics

--- services/garbage_collector/source/code/job_scheduler.py
+++ services/garbage_collector/source/code/job_scheduler.py
@@ -6,19 +6,23 @@ import garbage_collector
 def start(minutes_interval: int = 5) -> None:
     """Start the job scheduler."""
 
+    # convert the minutes interval to an integer
+    minutes_interval = int(minutes_interval)
+
+    # no need to run the job scheduler
     if minutes_interval <= 0:
-        # run immidiately
+        print('Collection task is chosen to run immidiately...')
         garbage_collector.collect()
         return
 
     print(f'Starting job scheduler to run every {minutes_interval} minutes...')
-
     # assign the job to the scheduler
     # to run every x minutes
     (
-        schedule.every(minutes_interval).minutes
+        schedule
+        .every(interval=minutes_interval).minutes
         .do(garbage_collector.collect)
-        .tag('garbage_collector')
+        .tag(garbage_collector.__name__)
     )
 
     # run the scheduler
