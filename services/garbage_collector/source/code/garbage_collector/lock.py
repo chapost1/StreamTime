961bd13024b5d9897d633e578667f6030d4b7d1f
Shahar Tal <chapo12345@gmail.com>
2022-12-28 00:40:56 +0200

    garbage_collector | added some comments to explain about the lock.py purpose

--- services/garbage_collector/source/code/garbage_collector/lock.py
+++ services/garbage_collector/source/code/garbage_collector/lock.py
@@ -7,21 +7,27 @@ import atexit
 
 logger = logging.getLogger(__name__)
 
-# TODO: fixme to use a better lock
-# i.e: redis distributed cluster
-# so that we can have multiple instances of the garbage collector
-# running at the same time
-# and they will be able to lock/unlock the garbage collector
-# without any issues
+# the garbage collector is a long running task
+# altough this task is important, it is not critical to occur at a specific time.
+# therefore there will be only one instance of the garbage collector running at a time.
+# Since the collect task is scheduled to run every x minutes, it is possible that multiple instances of the task will be triggered concurrently.
+# it may be possible that the garbage collector is already running (did not finish the previous trigger) when the task is triggered.
+# to prevent this, we will use a lock file to indicate if the garbage collector is already running.
+# if the garbage collector is already running, we will not run it again.
+
 
 # thread safe resource lock
 thread_safe_resource_lock = threading.Lock()
 
-
 class LockStatus(str, Enum):
     """The lock status."""
-    LOCKED = 'LOCKED'
-    UNLOCKED = 'UNLOCKED'
+    LOCKED = 1
+    UNLOCKED = 0
+
+
+def binary_lock_status(lock_status: LockStatus) -> bytes:
+    """Get the binary lock status."""
+    return bytearray(str(lock_status.value), encoding='utf-8')
 
 
 def get_lock_path() -> str:
@@ -42,9 +48,9 @@ def lock() -> bool:
 
     # check if the garbage collector is already locked
     if os.path.exists(get_lock_path()):
-        with open(get_lock_path(), 'r+') as lock_file:
+        with open(get_lock_path(), 'r+b') as lock_file:
             lock = lock_file.read()
-            if lock == str(LockStatus.LOCKED.value):
+            if lock == binary_lock_status(LockStatus.LOCKED):
                 logger.info('The garbage collector is already locked.')
                 # release the lock
                 thread_safe_resource_lock.release()
@@ -54,8 +60,8 @@ def lock() -> bool:
     
     logger.info('Locking the garbage collector...')
 
-    with open(get_lock_path(), 'w') as lock_file:
-        lock_file.write(LockStatus.LOCKED.value)
+    with open(get_lock_path(), 'wb') as lock_file:
+        lock_file.write(binary_lock_status(LockStatus.LOCKED))
 
     # release the lock
     thread_safe_resource_lock.release()
@@ -71,8 +77,8 @@ def unlock() -> None:
     # acquire the lock
     thread_safe_resource_lock.acquire()
     
-    with open(get_lock_path(), 'w') as lock_file:
-        lock_file.write(LockStatus.UNLOCKED.value)
+    with open(get_lock_path(), 'wb') as lock_file:
+        lock_file.write(binary_lock_status(LockStatus.UNLOCKED))
 
     # release the lock
     thread_safe_resource_lock.release()
